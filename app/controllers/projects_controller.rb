e656cd0ad5e282b72b0e850014714922b4df4c0c
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-02-25 14:09:02 -0500

    Added a projects controller and view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,36 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def new
+    @project = Project.new
   end
 
   def destroy
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :user_id, :goal_in_cents, :start_date, :end_date, :description, :image)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ActiveRecord::Base
+
+belongs_to :user
+has_many :users, through: :pledges
+has_many :pledges
 end
