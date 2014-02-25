6f4dc867223599e53958f5dfab800e3f32b5ff73
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-02-25 17:05:28 -0500

    Changed sessions permission from .id to just inheriting the object

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.user_id = current_user.id
+    @project.user = current_user
     if @project.save
       redirect_to project_path(@project)
     else
