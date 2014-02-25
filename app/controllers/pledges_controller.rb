c85c8a6460a488f6449837db0c588c023a869ba2
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-02-25 18:59:33 -0500

    Added a pledge view inside our project show view. Now you can add pledges and associate them with a specific project. (in activerecord only so far).

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,45 @@
 class PledgesController < ApplicationController
+	before_filter :load_project
+
+	  def index
+	    @pledges = Pledge.all
+	  end
+
+	  def create
+
+	    @pledge = Pledge.new(pledge_params)
+	    @pledge.project = @project
+	    if @pledge.save
+	      redirect_to pledges_path(@pledge)
+	    else
+	      render :new
+	    end
+	  end
+
+	  def update
+	  end
+
+	  def new
+	    @pledge = Pledge.new
+	  end
+
+	  def destroy
+	  end
+
+	  def show
+	    @pledge = Pledge.find(params[:id])
+	  end
+
+	  def edit
+	  end
+
+	  private
+	  def pledge_params
+	    params.require(:pledge).permit(:title, :description, :amount_in_cents, :project_id)
+	  end
+
+	  def load_project
+	  	@project = Project.find(params[:project_id])
+		end
+
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.user = current_user
+    @project.owner = current_user
     if @project.save
       redirect_to project_path(@project)
     else
