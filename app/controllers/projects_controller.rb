27616a347c2b1fcd477d4785344d3085fa089183
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-02-25 13:32:22 -0500

    Added projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,22 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
