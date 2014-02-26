8dd7182574f0384a5792b11754b0962665e0842b
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-02-26 18:54:27 -0500

    Added a backer_id to the has_many relationship of User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
 	has_many :projects, foreign_key: "owner_id"
 
 	has_many :breakpoints, :through => :pledges, :source => :backer
-	has_many :pledges
+	has_many :pledges, foreign_key: "backer_id"
 end
