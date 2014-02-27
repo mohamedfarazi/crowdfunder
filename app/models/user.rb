0777a4b10696435174dce4eab43c8886ab2f16be
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-02-26 21:17:26 -0500

    added user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	validates :email, uniqueness: {case_sensitive: false, message: "already exists in our system"}
 	has_many :projects, foreign_key: "owner_id"
 
-	has_many :breakpoints, :through => :pledges, :source => :backer
+	has_many :breakpoints, :through => :pledges
 	has_many :pledges, foreign_key: "backer_id"
 end
 
