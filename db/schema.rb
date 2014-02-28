ef6729c2053fcb94612153095c5726bcab72ff8f
Natalie Jubb <natalie.jubb@gmail.com>
2014-02-28 17:03:37 -0500

    trying to resolve merge conflict in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228173318) do
+ActiveRecord::Schema.define(version: 20140228201918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 20140228173318) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "last_logged_in"
+    t.boolean  "intro"
   end
 
 end
