e673041be3edd75dc4c465ce1ff81016523eed0f
Natalie Jubb <natalie.jubb@gmail.com>
2014-03-02 14:57:39 -0500

    Added newest first scope to project model and index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all
+    @projects = Project.all.newest_first
   end
 
   def create
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-
+scope :newest_first, -> { order(created_at: :desc) }
 belongs_to :owner, class_name: "User"
 has_many :backers, :through => :pledges
 has_many :breakpoints
