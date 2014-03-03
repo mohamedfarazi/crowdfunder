b4134afa422aa99230a593fd70c2accadafc0953
Natalie Jubb <natalie.jubb@gmail.com>
2014-03-02 20:52:50 -0500

    case insensitive email lookup on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_email(params[:email])
+  	user = User.where("email ILIKE ?", params[:email]).first
   	if user and user.authenticate(params[:password])
        user.last_logged_in = DateTime.now
        user.save
